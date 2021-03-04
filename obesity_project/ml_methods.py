e5aadbfdb8fa683032c5f00783114a472d0dd4b6
Jose Vilchez <70193228+jlvp07@users.noreply.github.com>
2021-03-04 05:11:48 -0500

    Completed Predictor

---
+++ obesity_project/ml_methods.py
@@ -0,0 +1,46 @@
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+def RunPrediction(form_inputs):
+    prediction_sample = np.array(form_inputs).reshape(1,-1)
+    error_flag, predicted_result =  predictDiabetes(prediction_sample)
+
+    return predicted_result 
+
+
+def predictDiabetes(prediction_sample) -> []: 
+
+    ### Initialize:
+    # Intialize error = No
+    error_flag = False
+    predicted_result = None
+
+    try:
+        # Predict result:
+
+        # Convert input to dataframe
+        input_df = pd.DataFrame(prediction_sample)
+
+        # Load the model from disk
+        # filename = "data/model_lin.sav"
+        # filename = "data/model_grb.sav"
+        filename = "data/model_rfr.sav"
+        # filename = "data/model_log.sav"
+        # filename = "data/model_knn.sav"
+        # filename = "data/model_svm.sav"
+        model = pickle.load(open(filename, 'rb'))
+
+        # Predict the result from model 
+        predicted_result = model.predict(input_df)
+        print(("Predicted result : {}").format(predicted_result))
+
+    except Exception as e:
+        print(('Exception occured : {}').format(e))
+        error_flag = True
+
+    
+    # Return
+    return error_flag, predicted_result
--- obesity_project/views.py
+++ obesity_project/views.py
@@ -3,9 +3,9 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from . import app
-
+from obesity_project.ml_methods import RunPrediction
 
 @app.route('/')
 @app.route('/home')
@@ -20,7 +20,26 @@ def model():
     return render_template('model.html')
 
 
-@app.route('/input')
-def input():
+@app.route('/predictor')
+def predictor():
     """Renders the prediction page."""
-    return render_template('input.html')
+    return render_template('predictor.html')
+
+@app.route('/results',methods = ['POST'])
+def results():
+    """Renders the prediction page."""
+    form_inputs = request.form.to_dict() 
+    form_inputs = list(form_inputs.values()) 
+    form_inputs = list(map(float, form_inputs)) 
+    result = RunPrediction(form_inputs)         
+    if int(result) == 0: 
+        prediction ='By our predictions, maybe it\'s genetics, maybe it\'s malnourishment... You may be Under Weight'
+    elif int(result) == 1:
+        prediction ='By our predictions, you\'re probably maintaining a Healthy Weight'            
+    elif int(result) == 2:
+        prediction ='By our predictions, your habits are likely to make you Overweight, if you aren\'t already.'            
+    elif int(result) == 3:
+        prediction ='By our predictions, you could be getting Obese, if you aren\'t already.'            
+    return render_template("results.html",
+             prediction = prediction
+             )
